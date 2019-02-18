9cb99148304a1fa3cb292b731cc1927ac2dd7cee
monarosa <rukal.61@gmail.com>
2019-02-17 23:22:52 -0500

    time is updated

--- Second/src/main/GoodMorning.java
+++ Second/src/main/GoodMorning.java
@@ -3,5 +3,6 @@ package main;
 public class GoodMorning {
 public static void main(String[] args) {
 	System.out.println("good morning");
+	System.out.println("good morning"+" it is still morning");
 }
 }
